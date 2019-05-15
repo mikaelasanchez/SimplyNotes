a8140651c3025d508c0158e5b07b565ca49febcb
Mikaela <mikaelaysanchez@gmail.com>
2019-05-15 01:10:52 +0100

    implement note saving with tags
    Signed-off-by: Mikaela <mikaelaysanchez@gmail.com>

--- src/com/funtech/java/Note.java
+++ src/com/funtech/java/Note.java
@@ -1,7 +1,13 @@
 package com.funtech.java;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static com.funtech.java.Main.println;
+import static com.funtech.java.Main.resourcePath;
 
 class Note {
     String title;
@@ -20,6 +26,27 @@ class Note {
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
         Date date = new Date();
         this.date = dateFormat.format(date);
+    }
+
+    public String toString(){
+        return this.title;
+    }
+
+    void save(){
+        try {
+            BufferedWriter noteSaver = new BufferedWriter(new FileWriter((resourcePath), true)); // true for append
+            noteSaver.write("<title>"+title+"</title>");
+            noteSaver.newLine();
+            noteSaver.write("<date>"+date+"</date>");
+            noteSaver.newLine();
+            noteSaver.newLine();
+            noteSaver.write("<content>"+contents+"</content>");
+            noteSaver.newLine();
+            noteSaver.write("--end of note--");
+            noteSaver.close();
+        }catch(IOException e){
+            println("Error: Cannot save the note.");
+        }
     }
 
     static void delete(){
